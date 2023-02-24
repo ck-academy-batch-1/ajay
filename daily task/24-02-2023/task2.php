8d0882c2b096d0ba57c63fb92957477284ad4c03
Ajaykiran <117974267+Ajaykiran@users.noreply.github.com>
2023-02-24 06:59:24 +0000

    Update task2.php

--- daily task/24-02-2023/task2.php
+++ daily task/24-02-2023/task2.php
@@ -1,23 +1,95 @@
-
-    <?php
-    echo "<pre>";
-    $rows = 7;
-    $cols = ($rows * 2) - 1;
-
-    for ($i = 1; $i <= $rows; $i++) {
-        for ($j = 1; $j <= $cols; $j++) {
-            $diff = abs($j - ($rows + 1));
-            $min = min($i - 1, $rows - $i);
-            if ($diff <= $min) {
-                echo ($min + 1 - $diff) * 3 . " ";
-            } else {
-                echo "&nbsp;&nbsp;";
-            }
-        }
-        echo "<br>";
+<style>
+    section {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        margin-top: 15%;
     }
-    echo"<pre>"
-    ?>
+</style>
 
 
 
+
+<section>
+
+
+    <div>
+        <?php
+        for ($i = 0; $i <= 7; $i++) {
+            for ($j = 0; $j <= 7; $j++) {
+                if ((($j == 1 or $j == 5) and $i != 0) or (($i == 0 or $i == 3) and ($j > 1 and $j < 5)))
+                    echo "*";
+                else
+                    echo "&nbsp ";
+                    if ((($j == 1 or $j == 5) and $i != 0) or (($i == 0 or $i == 3) and ($j > 1 and $j < 5)))
+                    echo "*";
+                else
+                    echo "&nbsp ";
+            }
+            echo "<br>";
+        }
+
+        ?>
+    </div>
+
+    <div>
+        <?php
+        for ($i = 0; $i < 8; $i++) {
+            for ($j = 0; $j <= 8; $j++) {
+                if (($j == 4 and $i != 6) or ($i == 0 and $j > 2 and $j < 6) or ($i == 6 and $j == 3) or ($i == 5 and $j == 2))
+                    echo "*";
+                else
+                    echo "&nbsp ";
+                    if (($j == 4 and $i != 6) or ($i == 0 and $j > 2 and $j < 6) or ($i == 6 and $j == 3) or ($i == 5 and $j == 2))
+                    echo "*";
+                else
+                    echo "&nbsp ";
+            }
+            echo "<br>";
+        }
+        ?>
+    </div>
+
+
+    <div>
+        <?php
+        for ($i = 0; $i <= 7; $i++) {
+            for ($j = 0; $j <= 7; $j++) {
+                if ((($j == 1 or $j == 5) and $i != 0) or (($i == 0 or $i == 3) and ($j > 1 and $j < 5)))
+                    echo "*";
+                else
+                    echo "&nbsp ";
+                    if ((($j == 1 or $j == 5) and $i != 0) or (($i == 0 or $i == 3) and ($j > 1 and $j < 5)))
+                    echo "*";
+                else
+                    echo "&nbsp ";
+            }
+            echo "<br>";
+        }
+
+        ?>
+    </div>
+
+
+    <div>
+        <?php
+        for ($i = 0; $i < 8; $i++) {
+            for ($j = 0; $j <= 8; $j++) {
+                if ((($j == 1 or $j == 5) and $i < 2) or $i == $j and $j > 0 and $j < 4 or ($j == 4 and $i == 2) or (($j == 3) and $i > 3))
+                    echo "*";
+                else
+                    echo "&nbsp ";
+                    if ((($j == 1 or $j == 5) and $i < 2) or $i == $j and $j > 0 and $j < 4 or ($j == 4 and $i == 2) or (($j == 3) and $i > 3))
+                    echo "*";
+                else
+                    echo "&nbsp ";
+            }
+            echo "<br>";
+        }
+        ?>
+    </div>
+
+
+
+
+</section>
